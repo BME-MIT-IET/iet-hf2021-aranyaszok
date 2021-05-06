e8b690c930ce33c0080fe8a113bab8a32363e4a9
bodoistvan <61791386+bodoistvan@users.noreply.github.com>
2021-05-06 13:51:22 +0200

    #init Junit test 5

---
+++ src/test/java/aranyaszok/MyFirstTest.java
@@ -0,0 +1,16 @@
+package test.java.aranyaszok;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+	@Test
+	@DisplayName("My first test should work") 
+	public void testAddittion4() {
+		assertEquals(true, true, "");   
+	}
+	
+}
